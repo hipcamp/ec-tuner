42f51faad6b3987536fbbc9f85b49400f568170a
Scott Goodell <scott@Scotts-MacBook-Pro.local>
2021-11-09 10:34:57 -0800

    empty while

--- src/main.ts
+++ src/main.ts
@@ -67,21 +67,7 @@ async function run(
 
           ec2.stopInstances(instanceIds)
 
-          let stoppedInstanceRunning = await ec2.anyStoppedInstanceRunning(
-            instancePrivateIps
-          )
-          core.warning(`Any instance running?: ${stoppedInstanceRunning}`)
-          while (stoppedInstanceRunning) {
-            setTimeout(async () => {
-              core.info('Waiting for required instances to go offline..')
-              stoppedInstanceRunning = await ec2.anyStoppedInstanceRunning(
-                instancePrivateIps
-              )
-              core.warning(
-                `Any instance running inside?: ${stoppedInstanceRunning}`
-              )
-            }, 5000)
-          }
+          while (await ec2.anyStoppedInstanceRunning(instancePrivateIps)) {}
           stoppedInstanceCount += instanceIds.length
         }
       }
