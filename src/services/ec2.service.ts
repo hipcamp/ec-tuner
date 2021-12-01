b4f78669015ae55b3bf44c46accf975b52fcd5f9
Caleb Gosiak <caleb@hipcamp.com>
2021-12-01 10:36:09 -0600

    introduce randomness to instance selection

--- src/services/ec2.service.ts
+++ src/services/ec2.service.ts
@@ -72,6 +72,26 @@ export class EC2Service {
     }
   }
 
+  private shuffle(array: unknown[]): unknown[] {
+    let currentIndex = array.length,
+      randomIndex
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+
+      // And swap it with the current element.
+      ;[array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex]
+      ]
+    }
+
+    return array
+  }
+
   async getFreeInstances(
     label: string,
     runners = 1,
@@ -89,7 +109,12 @@ export class EC2Service {
       })
 
       if (filteredInstances.length > 0) {
-        resolve(filteredInstances.slice(0, runners))
+        resolve(
+          (this.shuffle(filteredInstances) as SimpleInstance[]).slice(
+            0,
+            runners
+          )
+        )
       } else {
         // TODO: Add messaging for when not enough runners are available, and potential retry logic
         // TODO: add some alerting here
